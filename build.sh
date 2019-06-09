a6654941f59bf678b9fc42ac07a6b1d8c47b5ac7
kazukazuinaina <ypa.ypa.ypa.t@gmail.com>
2019-06-09 14:54:12 +0900

    add file

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 cd ~/vim/src
 ./configure --with-features=huge --enable-gui=gtk2 --enable-perlinterp --enable-pythoninterp --enable-python3interp --enable-rubyinterp --enable-luainterp --enable-fail-if-missing
-make
+sudo make
 sudo make install
