6e28cb62543dcc94b702c44f9514d6a4feedf9d0
kazukazuinaina <ypa.ypa.ypa.t@gmail.com>
2019-08-09 21:29:14 +0900

    update auto build tool

--- build.sh
+++ build.sh
@@ -20,12 +20,26 @@ if [ ! -e ${proj_name} ]; then
 else
     # pullもしくはfetch＆reset --hardを使う
     # 競合を起こさないよう、強制的に最新のリモートリポジトリに合わせる
-    sudo git pull origin ${branch}
-    echo 'git pull success!'
-fi
-# build
-cd ~/vim/src
-echo 'start build vim'
-./configure --with-features=huge --enable-gui=gtk2 --enable-perlinterp --enable-pythoninterp --enable-python3interp --enable-rubyinterp --enable-luainterp --enable-fail-if-missing
-sudo make
-sudo make install
+    result=`sudo git pull origin ${branch}`
+    if [ "`echo $result | grep 'Already'`" ]; then
+        echo 'Already up to date'
+        echo 'finish'
+    else
+        echo 'git pull is success!'
+        # build
+        cd ~/vim/src
+        echo 'start build vim OK? [yes/no]'
+        read answer
+        if [ "$answer" == "yes" ]; then
+            ./configure --with-features=huge\
+                --enable-gui=gtk2 --enable-perlinterp --enable-pythoninterp\
+                --enable-python3interp --enable-rubyinterp --enable-luainterp\
+                --enable-fail-if-missing\
+                sudo make
+                sudo make install
+                echo 'build done!'
+            else
+                echo 'finish'
+                fi
+        fi
+    fi
